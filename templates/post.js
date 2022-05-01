800b2b31223d1862d9e153f475f0d25c9b572738
Matt Rogan <mr1360@york.ac.uk>
2022-05-01 19:05:57 +0100

    Created script file & added into HTML source code

---
+++ templates/post.js
@@ -0,0 +1,11 @@
+/**
+ * Functionality for post pages will go in here
+ */
+
+function newPost() {
+    // Function to get data for a new post
+}
+
+function goBack() {
+    // Function for button to go back to previous page
+}
