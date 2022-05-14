dbeaad8d55a16d20572d0cde31bc01acf4cd9507
Matt Rogan <mr1360@york.ac.uk>
2022-05-14 13:00:54 +0100

    Updated database name and structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, redirect, render_template, url_for, request
 app = Flask(__name__)
 
 def get_db_connection():
-   conn = sqlite3.connect("forum_posts.db")
+   conn = sqlite3.connect("database.db")
    conn.row_factory = sqlite3.Row
    return conn
 
